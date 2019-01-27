9906222a9bf05559e7afbc3f358212d44ada9fb4
tiff-wang <wangtiffany@gmail.com>
2019-01-27 12:37:28 -0800

    no hardcoding lol

--- server/api/googleMapsController.js
+++ server/api/googleMapsController.js
@@ -1,10 +1,34 @@
 const decodePolyline = require('decode-google-map-polyline');
 
+const AZURE_BLOB_URL = 'https://pathplanning.blob.core.windows.net/tenderloin/pathplanning'
+var danger_data = ''
+var request = require('request');
+request(AZURE_BLOB_URL, function (error, response, body) {
+   if (!error && response.statusCode == 200) {
+      danger_data = JSON.parse(body);
+      console.log(danger_data)
+   }
+})
+
 test_polyline = 'y_reF|yejVmA_Rk@_IsAgTqAoSa@kHBo@Tm@BSt@gACGHKfAyAvAkBgFcHaDoEwBwCwAqBoAcBGQi@y@OSi@u@iA}AsBoCy@mAMKMGeBcCaC}CoBmCwHgKoKaO}B}C_AoArFwHrFuHzAsB'
 
 module.exports = {
     findBestRoute: (routes) => {
-        var points = decodePolyline(test_polyline)
-        return points
+        var min_score = Number.MAX_SAFE_INTEGER
+        var bestRoute = decodePolyline(routes[0].overview_polyline.points)
+        routes.forEach(function(route) {
+            var points = decodePolyline(route.overview_polyline.points)
+            var score = 0
+            points.forEach(function(point) {
+                key = 'lat: ' + point.lat.toFixed(5) + ',lng: ' + point.lng.toFixed(5)
+                console.log(key)
+                score += danger_data[key]
+            })
+            if (score < min_score) {
+                min_score = score
+                bestRoute = points
+            }
+        })
+        return bestRoute
     }
 }
